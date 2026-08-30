161eb4442ca2d1d2dce35dbaa37ee4f4addcdedd
botpiotr <delphi3.pierre@gmail.Com>
2026-08-30 18:19:57 +0200

    ajout des fichiers de base (Hello World)

---
+++ Game_1/mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    qDebug() << "Hello World !!";
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
